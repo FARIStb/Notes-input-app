53dd5f9acfa37cced75fa806371e8d06c6a121a7
FARIStb <farisadam105@gmai.com>
2025-02-26 10:36:16 +0530

    Notes

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Notes from './components/Notes'
+
+function App() {
+
+  return (
+    <>
+      <div className="a">
+        <Notes/>
+      </div>
+    </>
+  )
+}
+
+export default App
